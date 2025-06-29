199b5997b6a07dd0ecc8ebbfe4ac9b2b7b82dc2a
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2025-06-29 09:20:04 +0700

    add handle function remove Personnel

--- src/app/controllers/manageController.js
+++ src/app/controllers/manageController.js
@@ -1,10 +1,23 @@
-
+const Person = require("../models/User");
 class manageController {
-
-    async personnel(req, res) {
-        res.render('personnel');
-    }
-
+  async personnel(req, res) {
+    const person = await Person.find().sort({ createdAt: -1 }).lean();
+    res.render("personnel", { person });
+  }
+  async removePersonnel(req, res) {
+    //cần phải check tài khảo đang đăng nhặp , có thể xoá nhầm tài khoản đang đăng nhập
+    Person.findById(req.params.id)
+      .then((personnel) => {
+        if (!personnel) {
+          return res
+            .status(404)
+            .json({ success: false, message: "Personnel not found" });
+        }
+        console.log(req.params.id);
+        return Person.deleteOne({ _id: req.params.id });
+      })
+      .then(() => res.redirect("back"));
+  }
 }
 
-module.exports = new manageController;
+module.exports = new manageController();
