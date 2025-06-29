ed1d59386173463a02d4f6f0ff6911eeed200e0e
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2025-06-29 09:22:42 +0700

    install moment and add helpers formatDate

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ const session = require("express-session");
 const path = require("path");
 const db = require("./config/db");
 const router = require("./routers");
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const methodOverride = require('method-override');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const methodOverride = require("method-override");
+const moment = require('moment');
 const port = 3000;
 const morgan = require("morgan");
 
@@ -28,17 +29,17 @@ app.use((req, res, next) => {
   next();
 });
 
-
 db.connect();
 
-app.use(cors({
-  origin: 'http://localhost:3000', 
-  credentials: true, // Cho phép gửi cookie
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, // Cho phép gửi cookie
+  })
+);
 
 app.use(express.json()); // Hỗ trợ JSON request
-app.use(methodOverride('_method')); //Middleware Xử Lý _method=DELETE(UPDATE) khi dùng form
-
+app.use(methodOverride("_method")); //Middleware Xử Lý _method=DELETE(UPDATE) khi dùng form
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -54,6 +55,9 @@ const hbs = create({
     eq: (a, b) => a === b,
     or: (a, b) => a || b,
     multiply: (a, b) => a * b,
+    formatDate: function (date) { //convert time
+      return moment(date).format("DD/MM/YYYY");
+    },
   },
 });
 
