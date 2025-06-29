cda9742f61f6955d4a4b14a02fbfc3da91a4317b
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-06-29 23:39:15 +0700

    update router form add personnel

--- src/routers/manage.js
+++ src/routers/manage.js
@@ -3,6 +3,7 @@ const manageController = require("../app/controllers/manageController");
 
 const router = express.Router();
 router.get("/", manageController.personnel);
+router.get("/personnel/form-add", manageController.addForm);
 router.delete("/personnel/:id", manageController.removePersonnel);
 
 
