aa3e8903576f80c7584644edcaeadb9f78de5a23
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2025-07-03 08:38:02 +0700

    create API form add

--- src/routers/manage.js
+++ src/routers/manage.js
@@ -5,6 +5,7 @@ const router = express.Router();
 router.get("/", manageController.personnel);
 router.get("/personnel/form-add", manageController.addForm);
 router.delete("/personnel/:id", manageController.removePersonnel);
+router.post("/personnel/add", manageController.addPersonnel);
 
 
 module.exports = router;
