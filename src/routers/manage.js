c77e97fb6d009d8cbcc58c5e649ef6da2c81d08b
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-07-06 21:38:07 +0700

    update API edit and update personnel

--- src/routers/manage.js
+++ src/routers/manage.js
@@ -7,5 +7,8 @@ router.get("/personnel/form-add", manageController.addForm);
 router.delete("/personnel/:id", manageController.removePersonnel);
 router.post("/personnel/add", manageController.addPersonnel);
 
+router.get("/personnel/edit/:id", manageController.editPersonnel);
+router.post("/personnel/update/:id", manageController.updatePersonnel);
+
 
 module.exports = router;
