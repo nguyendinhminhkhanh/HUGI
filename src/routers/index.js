bc462674f8179e44192d87b74ee3d0acc393ab5e
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-07-13 17:02:06 +0700

    router profile

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,11 +2,13 @@ const home = require("./home");
 const auth = require("./auth");
 const concept = require("./concept");
 const manager = require("./manage");
+const profile = require("./profile");
 function router(app) {
+  app.use("/profile", profile);
   app.use("/concept", concept);
   app.use("/manager", manager);
   app.use("/auth", auth);
   app.use("/", home);
-}
+} 
 
 module.exports = router;
