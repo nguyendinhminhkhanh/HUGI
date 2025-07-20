af6b0f1bf6dcb6d4832ccfc48a128c9ae4cf3615
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-07-20 22:13:24 +0700

    create API mailbox

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,12 +3,14 @@ const auth = require("./auth");
 const concept = require("./concept");
 const manager = require("./manage");
 const profile = require("./profile");
+const mailbox = require("./mailbox");
 function router(app) {
   app.use("/profile", profile);
   app.use("/concept", concept);
   app.use("/manager", manager);
+  app.use("/mailbox", mailbox);
   app.use("/auth", auth);
   app.use("/", home);
-} 
+}
 
 module.exports = router;
