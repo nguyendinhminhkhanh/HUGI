e1f73e1b243e288881d2c89a99f4b40602e2cbe8
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-07-20 22:15:46 +0700

    create router mailbox.js

---
+++ src/routers/mailbox.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const mailboxController = require("../app/controllers/mailboxController");
+
+const router = express.Router();
+router.get("/", mailboxController.inbox);
+router.get("/mail_compose", mailboxController.mail_compose);
+
+module.exports = router;
