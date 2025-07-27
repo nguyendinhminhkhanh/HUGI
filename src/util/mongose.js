cda6b7d7fccc7f7c9d3e7ef9cd14f95f81e4c99b
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-07-27 21:31:29 +0700

    update mogose to object

--- src/util/mongose.js
+++ src/util/mongose.js
@@ -1,11 +1,19 @@
 module.exports = {
-  mutipleMongooseToObject: function (mongoose) {
-    return mongoose.map((mongoose) => mongoose.toObject());
+  mutipleMongooseToObject: function (mongooseArray) {
+    return Array.isArray(mongooseArray)
+      ? mongooseArray.map((item) =>
+          typeof item.toObject === "function" ? item.toObject() : item
+        )
+      : mongooseArray;
   },
+
   mongooseToObject: function (mongoose) {
-    return mongoose ? mongoose.toObject() : mongoose;
+    return mongoose && typeof mongoose.toObject === "function"
+      ? mongoose.toObject()
+      : mongoose;
   },
 };
+
 //multipleMongooseToObject() là hàm tiện ích (utility)
 //dùng để chuyển mảng dữ liệu từ Mongoose về dạng JavaScript "thuần" (plain object)
 //điều này bắt buộc nếu bạn dùng Handlebars (hbs) để render dữ liệu trong view.
