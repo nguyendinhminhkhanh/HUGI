b7a271d0f45cfa0674f4833ecc7d5885e7b68fdc
nguyendinhminhkhanh <mr.dinhkhanh18@gmail.com>
2025-07-27 21:32:19 +0700

    create API /profile/editAvatar

--- src/routers/profile.js
+++ src/routers/profile.js
@@ -3,6 +3,7 @@ const profileController = require("../app/controllers/profileController");
 
 const router = express.Router();
 router.get("/", profileController.profile);
+router.post("/editAvatar", profileController.editAvatar);
 
 
 module.exports = router;
