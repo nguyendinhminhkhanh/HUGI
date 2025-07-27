24fad9cd8476e3a72095edb5ed5ec1691ea2738d
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2025-07-27 09:44:31 +0700

    update api chat

--- src/routers/mailbox.js
+++ src/routers/mailbox.js
@@ -4,5 +4,6 @@ const mailboxController = require("../app/controllers/mailboxController");
 const router = express.Router();
 router.get("/", mailboxController.inbox);
 router.get("/mail_compose", mailboxController.mail_compose);
+router.get("/chat", mailboxController.chat);
 
 module.exports = router;
