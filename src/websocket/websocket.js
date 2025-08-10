b10907b9bbb2ad149c6feeacb3636c795b64d0f0
Nguyen Dinh Minh Khanh <mr.dinhkhanh18@gmail.com>
2025-08-10 13:23:13 +0700

    cau hinh server ket noi websocket

--- src/websocket/websocket.js
+++ src/websocket/websocket.js
@@ -1,6 +1,6 @@
 const socketIo = require("socket.io");
 
-function websocketServer(server) {
+function websocketServer(server, sessionMiddleware) {
   const io = socketIo(server, {
     cors: {
       origin: "http://localhost:3000",
@@ -8,14 +8,20 @@ function websocketServer(server) {
       credentials: true,
     },
   });
+
+  // Chuyển express-session middleware vào socket.io
+  io.use((socket, next) => {
+    sessionMiddleware(socket.request, {}, next);
+  });
+
   io.on("connection", (socket) => {
+    // const session = socket.request.session;
     console.log("1 người  đã kêt nối ", socket.id);
-    socket.on("setUserName", (username) => {
-      socket.username = username;
-      console.log(username);
-    });
+    // console.log(session.existingUser.name);
     socket.on("sendMessage", (msg) => {
-      io.emit("chatMessage", { username: socket.username, msg });
+      console.log("👤 Người gửi:", msg.username);
+      console.log("💬 Nội dung :", msg.message);
+      io.emit("chatMessage", msg);
     });
     socket.on("disconnect", () => {
       console.log("1 người đã thoát ");
